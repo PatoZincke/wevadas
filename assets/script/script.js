8d840a552f09769edc279a1bf20604496507b0da
PatoZincke <patricio.zincke@gmail.com>
2017-11-14 22:30:42 -0300

    Version 1.0
    Feed de Noticias en Particulas & Interface Basica

---
+++ assets/script/script.js
@@ -0,0 +1,53 @@
+(function(){
+
+	var loadingImg	=	$(".loading"),
+		header		=	$(".header");
+
+
+	function sourcesList(lista) {
+
+		console.log(lista);
+
+		var sourceTemplate 	= 	document.getElementById("source-template").innerHTML,
+			sourceCompiled	=	Handlebars.compile(sourceTemplate),
+			sourceRender	=	sourceCompiled(lista),
+			fuente	 		= 	document.getElementById("source-list");
+
+			fuente.innerHTML = sourceRender;
+	}
+
+	var apiUrl 		= 	"https://newsapi.org/v1/sources?language=en&",
+		key 		= 	"apiKey=2a3d0185f6844aec8deae27977faa16d",
+		url2		=	apiUrl + key,
+		url 		= 	"https://newsapi.org/v1/articles?source=engadget&apiKey=2a3d0185f6844aec8deae27977faa16d";
+
+
+	$.ajax({
+	  url: url,
+	  dataType: "json",
+	  success: function(data) {
+	    sourcesList({ lista: data.articles });
+	    loadingImg.fadeOut("fast");
+	  }
+	});
+
+	$(window).on('scroll', function() {
+
+	   	var st = $(this).scrollTop();
+
+	   	if( st > 20){
+
+	   		header.addClass("header-shadow");
+
+	   	} else if( st < 20){
+	   		
+	   		header.removeClass("header-shadow");
+	   	
+	   	}
+
+	 });
+
+	
+})();
+
+
